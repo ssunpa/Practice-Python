6cad65e9f3bea321725fff4d740793d11c7b384a
ssunpa <bestpa.1130@gmail.com>
2021-09-06 20:57:58 +0900

    Add: CH03 예제 코드

---
+++ CH03/03-02strslicing.py
@@ -0,0 +1,9 @@
+str = 'Monty python'
+
+print('문자열의 길이', len(str))
+print('str[0:5] :',str[0:5])
+print('str[6:] :',str[6:])
+print('str[6:len(str)] :', str[6:len(str)])
+print('str[-len(str):-7] :',str[-len(str):-7])
+print('str[-6:] :',str[-6:])
+print('str[-6:0] :',str[-6:0])
